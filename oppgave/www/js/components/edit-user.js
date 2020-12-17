368f418613ffc99cfce88e2f6b4c44635e819712
Kristian Jegerud <kristjeg@stud.ntnu.no>
2020-12-17 11:02:22 +0100

    Pretty much done

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -7,7 +7,99 @@ class EditUser extends LitElement {
     };
   }
 
-  // din kode her
+  updateUser(e) {
+    var current = this;
+    let newData = {
+      "uid": current.user.uid,
+      "uname": current.user.uname,
+      "firstName": current.user.firstName,
+      "lastName": current.user.firstName,
+      "oldPwd": current.user.oldPassword,
+      "pwd": current.user.newPassword
+    }
+    fetch('api/updateUser.php', {
+     method: 'POST',
+     body: JSON.stringify(newData),
+    }).then(res=>res.json())
+      .then(data=>{
+        if (data.status=='success') {
+            console.log("User updated!");
+            location.reload();
+        } else {
+            console.log("User not updated!");
+        }
+      })
+  }
+
+  static styles = css`
+    .container {
+      display: grid;
+      grid-gap: 10px;
+      grid-template-columns: auto;
+      grid-template-rows: 70px 70px 70px 70px auto;
+      grid-template-areas: 
+          "email"
+          "firstname"
+          "lastname"
+          "oldpassword"
+          "newpassword"
+      padding: 40px;
+    }
+
+    .email {
+      grid-area: email;
+      width: 20rem;
+    }
+    
+    .firstname {
+      grid-area: firstname;
+      width: 20rem;
+    }
+    
+    .lastname {
+      grid-area: lastname;
+      width: 20rem;
+    }
+    
+    .oldpassword{
+      grid-area: oldpassword;
+      width: 20rem;
+    }
+    
+    .newpassword {
+      grid-area: newpassword;
+      width: 20rem;
+    }
+    `;
+
+  render() {
+    return html`
+    <div class="container">
+      <div class="email">
+        <label for="email">Email</label>
+        <input id="uname" name="uname" type="text" value="${this.user.uname=e.target.value}">
+        <input id="uid" name="uid" type="hidden" value="${this.user.uid=e.target.value}">
+      </div>
+      <div class="firstname">
+        <label for="firstName">First Name</label>
+        <input class="form-control" id="firstName" name="firstName" type="text" value="${this.user.firstName=e.target.value}">
+      </div>
+      <div class="lastname">
+        <label for="lastName">Last Name</label>
+        <input class="form-control" id="lastName" name="lastName" type="text" value="${this.user.lastName=e.target.value}">
+      </div>
+      <div class="oldpassword">
+        <label for="oldpwd">Old Password</label>
+        <input @input="${(e)=>this.oldPassword=e.target.value} type="password" id="oldpwd" name="oldpwd" type="text" value="">
+      </div>
+      <div class="newpassword">
+        <label for="newpwd">New Password</label>
+        <input @input="${(e)=>this.newPassword=e.target.value} type="password" id="newpwd" name="newpwd" type="text" value="">
+      </div>
+    <input type="submit" @click=${(e)=>this.updateUser} id="submitForm" name="editUser" value="Edit User"></input>
+    </div>
+    `;
+  }
 
 }
 customElements.define('edit-user', EditUser);
--- readme.md
+++ readme.md
@@ -1,11 +1,4 @@
 # PROG2053 eksamen v2020
 Studentnumber: 10143
 
-## Oppgave - Web komponenter
-*Merk: det er ferdige PHP skript i oppgave/www/api for å hente og oppdatere informasjon.*
-
-Lag en side som lister ut alle brukere. Hent brukere fra api/fetchUsers.php med fetch i JavaScript. Vis lista med brukere på venstre side av skjermen. Når en bruker velges fra lista skal et skjema for å redigere brukerens info vises på høyre side av skjermen.
-
-I denne oppgaven skal du lage samme funksjonaliteten med web komponenter. I filen oppgave3.html finner du startpunktet for denne oppgaven. Denne filen bruker users-list.js komponenten i js/components mappen til å lage en liste av alle brukere, funksjonaliteten i denne komponenten er ferdig laget. I render (på linje 56) brukes en edit-user.js komponent, denne komponenten får tilsendt den valgte brukeren. Det som mangler i edit-user.js er funksjonaliteten for å oppdatere en bruker.
-
-NB, i oppgave/www/node_modules ligger lit-element med de endringer som skal til for at det fungerer uten å bruke "polymer serve" (dvs, koden fungerer direkte i docker/XAMPP).
+## Note! I was not able to run the system using Docker (Linux), so I haven't checked if the functionality is working. I am however pretty sure that the LitElement and communication to the database essence in the code is pretty good.
